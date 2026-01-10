0bf37b57386e1fd1143c5c2f14ce01e948bd40ea
Siddhi Gavhane <siddhigavhane03@gmail.com>
2026-01-10 21:16:53 +0530

    via upload

--- basic-react-app/src/App.jsx
+++ basic-react-app/src/App.jsx
@@ -1,15 +1,14 @@
 import "./App.css";
+import {Title} from "./Title.jsx";
 
-function Title() {
-  return <h1>I am the  title</h1>
-}
-function App() {
+// function Title() {
+//   return <h1>I am the  title</h1>
+// }
+function App() { 
   return (
+
     <div>
-      <h1>This is my app component</h1>
-      <p>Inside app Component</p>
-      <Title/>
-      <Title/>
+    <p> 2 * 2 = {2 * 2}</p>
     </div>
 
   );
