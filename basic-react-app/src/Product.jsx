3f3ac2dbe087565b2e2be18ea66cab36b511266d
Siddhi Gavhane <siddhigavhane03@gmail.com>
2026-01-11 19:12:51 +0530

    via upload

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -1,11 +1,14 @@
 import "./Product.css";
 
-function Product(title) {
-    //console.log(props.title);
+function Product({ title, price, features, features2 }) {
+    console.log(features);
     return (
         <div className="Product">
             <h3>Product Title : {title}</h3>
-            <h5>Product Description</h5>
+            <p>Product Price : {price}</p>
+            <p>features : {features}</p>
+            <p>features : {features2}</p>
+
         </div>
     );
 }
--- basic-react-app/src/productTab.jsx
+++ basic-react-app/src/productTab.jsx
@@ -1,10 +1,12 @@
 import Product from "./Product.jsx";
 function productTab() {
+    let  option = ["fast","reliable","hi-tech"];
+    let option2 = {a:"fast", b:"reliable"};
     return (
         <>
-            <Product title="phone"></Product>
-            <Product title="laptop"></Product>
-            <Product title="ipad"></Product>
+            <Product title="phone" price="36000" features={option}></Product>
+            <Product title="laptop" price="50000" features2={option2}></Product>
+            <Product title="ipad" price="150000"></Product>
         </>
     );
 }
