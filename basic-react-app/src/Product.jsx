90e1cab2e9c1b0be3776cffa028a293ede91685d
Siddhi Gavhane <siddhigavhane03@gmail.com>
2026-01-11 19:41:37 +0530

    via upload

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -1,14 +1,11 @@
 import "./Product.css";
 
-function Product({ title, price, features, features2 }) {
-    console.log(features);
+    function Product({ title, price, features={} }) {
     return (
         <div className="Product">
             <h3>Product Title : {title}</h3>
             <p>Product Price : {price}</p>
-            <p>features : {features}</p>
-            <p>features : {features2}</p>
-
+            <p>{features.a}</p>
         </div>
     );
 }
--- basic-react-app/src/productTab.jsx
+++ basic-react-app/src/productTab.jsx
@@ -1,14 +1,13 @@
 import Product from "./Product.jsx";
-function productTab() {
-    let  option = ["fast","reliable","hi-tech"];
-    let option2 = {a:"fast", b:"reliable"};
-    return (
-        <>
-            <Product title="phone" price="36000" features={option}></Product>
-            <Product title="laptop" price="50000" features2={option2}></Product>
-            <Product title="ipad" price="150000"></Product>
-        </>
-    );
+
+function ProductTab() {
+  return (
+    <>
+      <Product title="phone" price={36000} features={{a:"fast", b: "durable"}} />
+      <Product title="laptop" price={50000} />
+      <Product title="ipad" price={150000} />
+    </>
+  );
 }
 
-export default productTab;
+export default ProductTab;
