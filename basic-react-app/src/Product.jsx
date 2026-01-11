5a145f898ed06297ef1250754124102fd574133e
Siddhi Gavhane <siddhigavhane03@gmail.com>
2026-01-11 23:58:15 +0530

    via upload

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -1,12 +1,13 @@
 import "./Product.css";
 
 function Product({ title, price, features = [] }) {
-
+   
     return (
         <div className="Product">
             <h3>Product Title : {title}</h3>
             <p>Product Price : {price}</p>
-            <p>{features.map((feature) => <li>{feature}</li>)}</p>
+            {/* <p>{features.map((feature) => <li>{feature}</li>)}</p> */}
+            <p>{ price > 80000 ? " Discount of 5 %" : null;}</p>
         </div>
     );
 }
